8f62fc271c84e6bddbf2c40541a6191287075294
Mayank <mayankghz225@gmail.com>
2023-06-10 12:36:05 +0530

    updated

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,8 @@ const Auth = () => {
 
     const { username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "http://localhost:5000/auth";
+    const URL = "https://chit-chat-7yhz.onrender.com/auth";
+    // const URL = "http://localhost:5000/auth";
 
     const {
       data: { token, userId, hashedPassword, fullName },
